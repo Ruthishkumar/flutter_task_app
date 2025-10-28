9549aa8080984ab4201a851a66b4dcc380f166c9
Ruthish <ruthish@deepsense.in>
2025-10-28 18:19:03 +0530

    Initial Commit

---
+++ lib/view/screen/crypto_view_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CryptoViewScreen extends StatefulWidget {
+  const CryptoViewScreen({super.key});
+
+  @override
+  State<CryptoViewScreen> createState() => _CryptoViewScreenState();
+}
+
+class _CryptoViewScreenState extends State<CryptoViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
